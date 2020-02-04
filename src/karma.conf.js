42043d6b3705fd64659534f2cf71a138f9ea595d
Mirza Medar <mirzame@maestralsolutions.com>
2020-02-04 10:28:42 +0100

    headless

--- src/karma.conf.js
+++ src/karma.conf.js
@@ -27,7 +27,7 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Chrome', 'PhantomJS'],
+    browsers: ['Chrome', 'PhantomJS', 'ChromeHeadless'],
     customLaunchers: {
       'PhantomJS_custom': {
         base: 'PhantomJS',
