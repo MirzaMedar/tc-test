d5f231d4509c635ac6e851604fd14fdf2cadb5f8
Mirza Medar <mirzame@maestralsolutions.com>
2020-02-04 13:03:55 +0100

    mime type

--- src/karma.conf.js
+++ src/karma.conf.js
@@ -6,23 +6,24 @@ module.exports = function (config) {
     basePath: '',
     frameworks: ['jasmine', '@angular-devkit/build-angular'],
     plugins: [
+      require('karma-teamcity-reporter'),
       require('karma-jasmine'),
       require('karma-chrome-launcher'),
       require('karma-jasmine-html-reporter'),
       require('karma-coverage-istanbul-reporter'),
       require('@angular-devkit/build-angular/plugins/karma'),
-      require('karma-teamcity-reporter'),
       require('karma-phantomjs-launcher')
     ],
     client: {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
+    mime: { 'text/x-typescript': ['ts','tsx'] },
     coverageIstanbulReporter: {
       dir: require('path').join(__dirname, '../coverage/tc-test'),
       reports: ['html', 'lcovonly', 'text-summary'],
       fixWebpackSourcePaths: true
     },
-    reporters: ['progress', 'kjhtml', 'teamcity'],
+    reporters: ['teamcity', 'progress', 'kjhtml'],
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
